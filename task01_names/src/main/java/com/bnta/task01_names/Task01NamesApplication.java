65a490ee7176cf063919b3d142d8dd69df0c88ab
Piraven Nandakumaran <Xothspath@gmail.com>
2023-10-31 19:51:36 +0000

    inital commit

---
+++ task01_names/src/main/java/com/bnta/task01_names/Task01NamesApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.task01_names;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task01NamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task01NamesApplication.class, args);
+	}
+
+}
