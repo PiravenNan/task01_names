17f48e0bca4babdd642f36a2b5222ca3a0ca2c2a
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-01 09:32:35 +0000

    Pojo

--- task01_names/src/main/java/com/bnta/task01_names/controllers/GreetingController.java
+++ task01_names/src/main/java/com/bnta/task01_names/controllers/GreetingController.java
@@ -1,17 +1,18 @@
 package com.bnta.task01_names.controllers;
 
+import com.bnta.task01_names.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/greeting") //localhost:8080/greeting
 public class GreetingController {
 
-    @GetMapping // GET localhost:8080/greeting
-
-    public String newGreeting(){
-        return "Hello";
+    @GetMapping// GET localhost:8080/greeting
+    public Greeting newGreeting(@RequestParam (defaultValue = "") String name,@RequestParam (defaultValue = "") String timeOfDay){
+        return new Greeting(name, timeOfDay);
     }
 
 }
